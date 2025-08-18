4aaf6205fa783fb0454709ecfb07f534c8ffaa9a
anupamsingh12 <anupmsingh12@gmail.com>
2025-08-18 10:13:52 +0530

    updared

--- src/components/product-page/Header/PhotoSection.tsx
+++ src/components/product-page/Header/PhotoSection.tsx
@@ -1,30 +1,29 @@
 "use client";
 
-import { Product } from "@/types/product.types";
 import Image from "next/image";
 import React, { useState } from "react";
 
-const PhotoSection = ({ data }: { data: Product }) => {
-  const [selected, setSelected] = useState<string>(data.srcUrl);
+const PhotoSection = ({ data }: { data: any }) => {
+  const firstVariantImages = data?.varients?.[0]?.images || [];
+  const [selected, setSelected] = useState<string>(firstVariantImages[0]);
 
   return (
     <div className="flex flex-col-reverse lg:flex-row lg:space-x-3.5">
-      {data?.gallery && data.gallery.length > 0 && (
+      {firstVariantImages.length > 0 && (
         <div className="flex lg:flex-col space-x-3 lg:space-x-0 lg:space-y-3.5 w-full lg:w-fit items-center lg:justify-start justify-center">
-          {data.gallery.map((photo, index) => (
+          {firstVariantImages.map((photo: string, index: number) => (
             <button
               key={index}
               type="button"
               className="bg-[#F0EEED] rounded-[13px] xl:rounded-[20px] w-full max-w-[111px] xl:max-w-[152px] max-h-[106px] xl:max-h-[167px] xl:min-h-[167px] aspect-square overflow-hidden"
               onClick={() => setSelected(photo)}
             >
-              <Image
+              <img
                 src={photo}
                 width={152}
                 height={167}
                 className="rounded-md w-full h-full object-cover hover:scale-110 transition-all duration-500"
-                alt={data.title}
-                priority
+                alt={data.name}
               />
             </button>
           ))}
@@ -32,14 +31,12 @@ const PhotoSection = ({ data }: { data: Product }) => {
       )}
 
       <div className="flex items-center justify-center bg-[#F0EEED] rounded-[13px] sm:rounded-[20px] w-full sm:w-96 md:w-full mx-auto h-full max-h-[530px] min-h-[330px] lg:min-h-[380px] xl:min-h-[530px] overflow-hidden mb-3 lg:mb-0">
-        <Image
+        <img
           src={selected}
           width={444}
           height={530}
           className="rounded-md w-full h-full object-cover hover:scale-110 transition-all duration-500"
-          alt={data.title}
-          priority
-          unoptimized
+          alt={data.name}
         />
       </div>
     </div>
